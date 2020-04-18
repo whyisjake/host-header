c18063884547358a6ea7f457d436b5f838ba18c0
Jake Spurlock <whyisjake@gmail.com>
2020-04-17 17:06:51 -0700

    Let's make this a lot simpler...

--- host-header.php
+++ host-header.php
@@ -15,14 +15,12 @@
  * @wordpress-plugin
  * Plugin Name:       Host Header
  * Plugin URI:        https://github.com/whyisjake/host-header
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Add a Host-Header to track down hosting partners in helping combat web spam.
  * Version:           1.0.0
  * Author:            Jake Spurlock
  * Author URI:        https://jakespurlock.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       host-header
- * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
@@ -31,52 +29,89 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
+ * Current plugin version.
  */
 define( 'HOST_HEADER_VERSION', '1.0.0' );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-host-header-activator.php
- */
-function activate_host_header() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-host-header-activator.php';
-	Host_Header_Activator::activate();
+function host_header_add_to_headers( $headers ) {
+	$options = get_option( 'host_header_api_settings' );
+
+	if ( '1' === $options['host_header_api_display_1'] && ! empty( $options['host_header_api_text_field_0'] ) || apply_filters( 'host_header_bypass', false ) ) {
+		$headers['Host-Header'] = apply_filters( 'host_header', $options['host_header_api_text_field_0'] );
+	}
+
+	return $headers;
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-host-header-deactivator.php
- */
-function deactivate_host_header() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-host-header-deactivator.php';
-	Host_Header_Deactivator::deactivate();
+// Add custom header for VIP
+add_filter( 'wp_headers', 'host_header_add_to_headers' );
+add_action( 'admin_menu', 'host_header_add_admin_menu' );
+add_action( 'admin_init', 'host_header_api_settings_init' );
+
+function host_header_add_admin_menu() {
+	add_options_page( 'Host Header Settings', 'Host Header Settings', 'manage_options', 'host-header-settings', 'host_header_settings' );
 }
 
-register_activation_hook( __FILE__, 'activate_host_header' );
-register_deactivation_hook( __FILE__, 'deactivate_host_header' );
+function host_header_api_settings_init() {
+	register_setting( 'host_header', 'host_header_api_settings' );
+	add_settings_section(
+		'host_header_api_host_header_section',
+		__( 'Options', 'Host_Header' ),
+		'host_header_api_settings_section_callback',
+		'host_header'
+	);
 
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-host-header.php';
+	add_settings_field(
+		'host_header_api_text_field_0',
+		__( 'Host Value', 'Host_Header' ),
+		'host_header_api_host_0_render',
+		'host_header',
+		'host_header_api_host_header_section'
+	);
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_host_header() {
-
-	$plugin = new Host_Header();
-	$plugin->run();
-
+	add_settings_field(
+		'host_header_api_display_1',
+		__( 'Display', 'Host_Header' ),
+		'host_header_api_select_field_1_render',
+		'host_header',
+		'host_header_api_host_header_section'
+	);
 }
-run_host_header();
+
+function host_header_api_host_0_render() {
+	$options = get_option( 'host_header_api_settings' );
+	?>
+	<input type='text' name='host_header_api_settings[host_header_api_text_field_0]' value='<?php echo $options['host_header_api_text_field_0']; ?>'>
+	<?php
+}
+
+function host_header_api_select_field_1_render() {
+	$options = get_option( 'host_header_api_settings' );
+	?>
+	<select name='host_header_api_settings[host_header_api_display_1]'>
+		<option value='1' <?php selected( $options['host_header_api_display_1'], 1 ); ?>>True</option>
+		<option value='0' <?php selected( $options['host_header_api_display_1'], 0 ); ?>>False</option>
+	</select>
+
+	<?php
+}
+
+function host_header_api_settings_section_callback() {
+	echo __( 'Choose whether to display the header, and what value to use.', 'Host_Header' );
+}
+
+function host_header_settings() {
+	?>
+	<form action='options.php' method='post'>
+
+		<h1>Host Header Settings</h1>
+
+		<?php
+		settings_fields( 'host_header' );
+		do_settings_sections( 'host_header' );
+		submit_button();
+		?>
+
+	</form>
+	<?php
+}
